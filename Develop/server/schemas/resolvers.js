553abce4278ad3bc6973f52523ed52ff4f3555e5
CECurtiss <curtissclinton@gmail.com>
2023-02-26 13:00:09 -0600

    wrote adduser, login, removebook for resolvers.js. addec signtoken, authenticationerror, and removed Book from require

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -1,4 +1,6 @@
-const { Book, User } = require("../models");
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -14,16 +16,45 @@ const resolvers = {
   },
 
   Mutations: {
-    saveBook: async (parent, { book }, context) => {
-      if (context.user) {
+    saveBook: async (parent, { _id, book }, { user }) => {
+      if (user) {
         const updatedBooks = await User.findOneAndUpdate(
-          { _id: context.user._id },
+          { _id },
           { $addToSet: { savedBooks: book } },
           { new: true, runValidators: true }
         );
         return updatedBooks;
       }
       throw new AuthenticationError("You must be logged in to do this");
+    },
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Can't find this user");
+      }
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Wrong password!");
+      }
+    },
+    removeBook: async (parent, { _id, bookId }, { user }) => {
+      if (user) {
+        const updatedUser = await User.findOneAndUpdate(
+          { _id },
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        );
+        return updatedUser;
+      }
+      throw new AuthenticationError("Couldn't find user with this id!");
     },
   },
 };
