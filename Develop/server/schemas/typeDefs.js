0772641149c8281362d592cc0e9d0cd4d5ad3bb1
CECurtiss <curtissclinton@gmail.com>
2023-02-25 21:53:53 -0600

    added some typeDef shapes

--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -1,1 +1,26 @@
-const { gql } = require('apollo-server-express')
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+
+input SaveBookInput {
+    description: String
+    bookID: String
+    image: String
+    link: String
+    authors: [String]
+}
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    saveBook(input: SaveBookInput!): User
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    deleteBook(bookId: String!): User
+}
+
+`;
+
+module.exports = typeDefs;
